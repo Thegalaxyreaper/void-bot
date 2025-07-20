95d94d5c7d04b3ecb1e179850292c98c9cdf2c0f
Thegalaxyreaper <princessjayd12@gmail.com>
2025-07-20 11:55:17 -0400

    index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ client.on("ready" , () => {
 client.on("messageCreate" , msg => {
     if (msg.content === "Welcome everyone please") {
         msg.reply("good evening everyone in the void! stream starts at 12 today!");
-    } else if  (msg.content === "Dante's wife") {
+    } else if  (msg.content === "Dantes wife") {
         msg.reply("Yukari has now been tossed into the void! RIP");
     } else if (msg.content === "Another bad yuka take") {
       msg.reply("Damn another bad take Yuka!");
